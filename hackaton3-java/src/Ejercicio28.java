66a9cfcf8d486d0bd7dcc3c17f656391c70dd7c9
aosaoye <a29322@svalero.com>
2025-10-23 13:59:23 +0200

    Ejercicio 28 Pendiente

---
+++ hackaton3-java/src/Ejercicio28.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Ejercicio28 {
+
+    /*
+    *Genera una matriz 6x6 con números del 1 al 9 y muestra solo el triángulo superior derecho.
+    */
+
+    public static void main(String[] args) {
+        int [][] matriz = new int[6][6];
+        Random random = new Random();
+
+        // Llenamos la matriz con ceros
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = 0;
+            }
+        }
+
+        //
+
+    }
+}
